115566ae6efbdcbae96435fecad62dbf5cec2791
S.Maham <syedahmaham@gmail.com>
2021-06-23 20:48:11 -0400

    added media queries 🎉

--- src/MealList.js
+++ src/MealList.js
@@ -1,12 +1,13 @@
 function MealList(props) {
   const {title, imgSrc, imgAlt, recipeUrl, calories, servings} = props;
+  let perServing = calories / servings;
   return (
     imgSrc != null &&
       (
       <li>
         <img src={imgSrc} alt={imgAlt} />
         <h2>{title}</h2>
-        <p>Calories: {calories} for {servings} serving(s). (Calories for single serving can be found in the link)</p>
+        <p>Total Calories: {calories} ({perServing.toFixed(1)} per serving.)</p>
         <a className="button" href={recipeUrl}> View full Recipe</a>
       </li>
       )
